c12f066fde319c53cdb1d44007c2695ec076680d
Anika Hussen <anika.hussen@gmail.com>
2019-07-09 11:47:35 -0400

    game in progress

---
+++ Assets/playerCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    
+        void OnCollisionEnter(Collision collisioninfo)
+        {
+            if (collisioninfo.gameObject.tag == "border")
+            {
+            Debug.Log("HIT");
+            }
+        }
+        
+
+
+
+}
