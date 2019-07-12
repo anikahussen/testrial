2d73db012f93469b1f36609bca3b98e639729d80
Anika Hussen <anika.hussen@gmail.com>
2019-07-12 11:33:59 -0400

    level01adds

---
+++ Assets/Scripts/RotateRose.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateRose : MonoBehaviour
+{
+    public int rotateSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
--- Assets/Scripts/bottleFloat.cs
+++ Assets/Scripts/bottleFloat.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class bottleFloat : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float min = 2f;
+    public float max = 3f;
     void Start()
     {
-        
+
+        min = transform.position.x;
+        max = transform.position.x + 3;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
     }
+
+
+
+
+
+
 }
